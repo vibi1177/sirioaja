82e5b71b31201df5f80b84051edf1cea5265945d
vibi1177 <vibi1177@gmail.com>
2018-07-19 11:17:04 +0200

    Prima creazione

---
+++ sirioaja/settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+BOT_NAME = 'sirioaja'
+
+SPIDER_MODULES = ['sirioaja.spiders']
+NEWSPIDER_MODULE = 'sirioaja.spiders'
+
+ROBOTSTXT_OBEY = True
+HTTPCACHE_ENABLED = True
---
+++ sirioaja/spiders/sirioaja.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SirioajaSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["sirioaja.it"]
+    start_urls = [
+        'https://sirioaja.it/scuola-per-estetista-contatti/,
+    ]
+
+    def parse(self, response):
+        yield{resonse.css('title::text').extract_first()}
